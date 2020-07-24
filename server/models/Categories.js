856bcb300b502e6fa32ef34423c9037f339e9978
CodyClark07 <codyclark2010@hotmail.com>
2020-07-24 09:33:32 -0600

    Category and Post Schema done

--- server/models/Categories.js
+++ server/models/Categories.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const Categories = new Schema(
     {
         title: { type: String, required: true },
-        name: { type: String, required: true },
         description: { type: String, required: true }
     },
     { timestamps: true, toJSON: { virtuals: true } }
--- server/models/Posts.js
+++ server/models/Posts.js
@@ -5,7 +5,11 @@ const Posts = new Schema(
   {
     title: { type: String, required: true },
     name: { type: String, required: true },
-    description: { type: String, required: true }
+    description: { type: String, required: true },
+    imgUrl: { type: Image, required: true },
+    comments: { type: String, required: true },
+    likes: { type: Number, required: true },
+    dislikes: { type: Number, required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
