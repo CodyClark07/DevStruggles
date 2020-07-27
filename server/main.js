61272fedbf0509f88b26d5b378a4bea24bccde9f
CodyClark07 <codyclark2010@hotmail.com>
2020-07-26 19:33:31 -0600

    mongo

--- server/main.js
+++ server/main.js
@@ -2,11 +2,11 @@ import express from "express";
 import Startup from "./Startup";
 import DbContext from "./db/DbConfig";
 
-const mongoose = require("mongoose")
+
 //create server & socketServer
 const app = express();
 const port = process.env.PORT || 3000;
-mongoose.connect(process.env.MONGODB_URI)
+
 
 
 
