e240e9813bb6aa6ec29536786bd457581c8cef15
CodyClark07 <codyclark2010@hotmail.com>
2020-07-26 19:07:13 -0600

    mongodb

--- server/main.js
+++ server/main.js
@@ -2,9 +2,17 @@ import express from "express";
 import Startup from "./Startup";
 import DbContext from "./db/DbConfig";
 
+const mongoose = require("mongoose")
 //create server & socketServer
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dev-struggles' {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+);
 
 //Establish Socket
 Startup.ConfigureGlobalMiddleware(app);
